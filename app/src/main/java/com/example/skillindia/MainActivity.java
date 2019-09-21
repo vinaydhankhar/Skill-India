5b29170707850157bfd6b389a7793d40ee0a47ad
Vinay Kumar <vinaydhankhar6@gmail.com>
2019-09-21 16:08:00 +0530

    login page

--- app/src/main/java/com/example/skillindia/MainActivity.java
+++ app/src/main/java/com/example/skillindia/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.skillindia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=findViewById(R.id.login);
+    }
+    public void login(View v){
+        Intent intent=new Intent(this,Login.class);
+        startActivity(intent);
     }
 }
